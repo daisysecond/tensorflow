4969e60607b55cb4a96275e2121be0a895e96d44
daisysecond <daisysecond@daisysecond.com>
2017-10-18 11:26:39 +0100

    WIP

--- gcp/push.sh
+++ gcp/push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-BASE_IMAGE_VERSION=v2
+BASE_IMAGE_VERSION=v3
 
 lines=`gcloud container images list-tags gcr.io/$PROJECT/t1 --filter="$BASE_IMAGE_VERSION" | wc -l`
 if [ $lines -eq 0 ]
