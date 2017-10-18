98790b8e3839a7b764ca4cc2d53cdbd2c3b10061
daisysecond <daisysecond@daisysecond.com>
2017-10-18 15:11:14 +0100

    WIP

--- gcp/push.sh
+++ gcp/push.sh
@@ -8,13 +8,13 @@ if [ $lines -eq 0 ]
 then
   echo "Building new base image version=$BASE_IMAGE_VERSION"
   gcloud container builds submit \
-    --timeout=30m \
+    --timeout=3h \
     --tag gcr.io/$PROJECT/t1:$BASE_IMAGE_VERSION \
     t1
 fi
 
 
 gcloud container builds submit \
-  --timeout=1h30m \
+  --timeout=2h \
   --tag gcr.io/$PROJECT/t2:$BASE_IMAGE_VERSION \
   t2
