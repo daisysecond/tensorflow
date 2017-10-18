e92e786fde14c16a58c7a3d6dc42945099cb5be7
daisysecond <daisysecond@daisysecond.com>
2017-10-18 12:03:55 +0100

    WIP

--- gcp/push.sh
+++ gcp/push.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-BASE_IMAGE_VERSION=v3
+BASE_IMAGE_VERSION=v4
 
 lines=`gcloud container images list-tags gcr.io/$PROJECT/t1 --filter="$BASE_IMAGE_VERSION" | wc -l`
 if [ $lines -eq 0 ]
 then
   echo "Building new base image version=$BASE_IMAGE_VERSION"
-  gcloud container builds submit --tag gcr.io/$PROJECT/t1:$BASE_IMAGE_VERSION t1
+  gcloud container builds submit \
+    --timeout=30m \
+    --tag gcr.io/$PROJECT/t1:$BASE_IMAGE_VERSION \
+    t1
 fi
 
 
