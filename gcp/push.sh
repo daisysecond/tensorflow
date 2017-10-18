062a72c86d935e3d7a1d198e407e811f2a119578
daisysecond <daisysecond@daisysecond.com>
2017-10-18 10:51:09 +0100

    WIP

---
+++ gcp/push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BASE_IMAGE_VERSION=v1
+
+lines=`gcloud container images list-tags gcr.io/$PROJECT/t1 --filter="$BASE_IMAGE_VERSION" | wc -l`
+if [ $lines -eq 0 ]
+then
+  echo "Building new base image version=$BASE_IMAGE_VERSION"
+  gcloud container builds submit --tag gcr.io/$PROJECT/t1:$BASE_IMAGE_VERSION t1
+fi
+
+
+gcloud container builds submit --tag gcr.io/$PROJECT/t2:$BASE_IMAGE_VERSION t2
